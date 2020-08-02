c7a1e81306d1d1d94c693add1794639808188390
nobu105 <dai9bensan@gmail.com>
2020-08-02 11:38:33 +0000

    ログイン後のリダイレクト修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-end
 
 
-   def after_sign_up_path_for(resource)
-    items_path
-  end
+   def after_sign_in_path_for(resource)
+      case resource
+      when Admin
+        admins_top_path
+      when Member
+        member_top_path
+      end
+   end
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :first_name_kana, :last_name, :last_name_kana, :postcode, :address, :phone_number, :email ])
   end
+
+end
