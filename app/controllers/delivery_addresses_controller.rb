ad07508064ec9379001ad421364b878fda92377c
ryotafff <jnblog2019@gmail.com>
2020-08-03 11:52:13 +0000

    Add delivery_address

--- app/controllers/delivery_addresses_controller.rb
+++ app/controllers/delivery_addresses_controller.rb
@@ -5,33 +5,32 @@ class DeliveryAddressesController < ApplicationController
    @member = current_member
    @delivery_address = DeliveryAddress.new
    @delivery_addresses = @member.delivery_addresses
-
-
   end
+
 
 #配送先新規保存
   def create
     @delivery_address = DeliveryAddress.new(delivery_address_params)
     @delivery_address.member_id = current_member.id
     if @delivery_address.save
-      flash[:success] = "新しい住所を登録しました"
+      flash[:notice] = "新しい住所を登録しました"
       redirect_to delivery_addresses_path
     else 
-      @member = current_user
+      @member = current_member
       @delivery_addresses = @member.delivery_addresses
       render :index
     end
   end
 
-
+#配送先削除
   def destroy
     @delivery_address = DeliveryAddress.find(params[:id])
     @delivery_address.destroy
-     flash[:success] = "登録した住所を削除しました。"
+     flash[:notice] = "登録した住所を削除しました。"
      redirect_to delivery_addresses_path
   end
 
-
+#配送先編集
   def edit
     @delivery_address = DeliveryAddress.find(params[:id])
     if @delivery_address.member != current_member
@@ -40,16 +39,21 @@ class DeliveryAddressesController < ApplicationController
   end
 
 
-
+#編集内容保存
   def update
+  @delivery_address = DeliveryAddress.find(params[:id])
 
+  if @delivery_address.update(delivery_address_params)
+    redirect_to delivery_addresses_path
+    flash[:notice]="更新しました。"
+  else
+   render :edit
   end
-
+end
 
 
 
  private
-
 
   def delivery_address_params
     params.require(:delivery_address).permit(:postcode, :address, :name,:member_id)
--- app/models/delivery_address.rb
+++ app/models/delivery_address.rb
@@ -4,7 +4,9 @@ class DeliveryAddress < ApplicationRecord
 belongs_to :member
 
 
-
+validates :postcode, presence: true, length: {minimum: 2, maximum: 10}
+validates :address, presence: true, length: {minimum: 2, maximum: 50}
+validates :name, presence: true, length: {minimum: 2, maximum: 50}
 
 
 end
