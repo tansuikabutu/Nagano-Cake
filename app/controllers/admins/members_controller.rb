ff3d2732d4b253227c14754e0e57bd2369f02029
nobu105 <dai9bensan@gmail.com>
2020-08-04 04:09:22 +0000

    ä¼　詳細ほぼ

--- app/controllers/admins/members_controller.rb
+++ app/controllers/admins/members_controller.rb
@@ -1,13 +1,27 @@
 class Admins::MembersController < ApplicationController
-  def edit
-  end
+  #ログインユーザーのみ
+  before_action :authenticate_admin!
 
   def index
+  	@members = Member.all
   end
 
   def show
+  	@member = Member.find(params[:id])
+  end
+
+  def edit
+  	@member = Member.find(params[:id])
   end
 
   def update
   end
+
+   private
+
+  def member_params
+    params.require(:member).permit(:is_status, :first_name, :first_name_kana, :last_name,:last_name_kana,:postcode,:address,:phone_number,:email)
+  end
+
+
 end
