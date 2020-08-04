26b77d174ff56d6769f1123b3d90c75b1f128acf
mihomatsuei <addictly_m@yahoo.co.jp>
2020-08-04 12:27:09 +0000

    Add strawberry

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,7 +44,7 @@ Item.create!(
     id: 1,
     genre_id: 1,
     name: "いちごのショートケーキ",
-    introduction: "長野県産のを使用した当店一押しの商品です",
+    introduction: "長野県産の苺を使用した当店一押しの商品です",
     price: 1000,
     is_status: 0,
     image: open("./db/images/image1.jpg")
