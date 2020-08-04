e4160b453f85a3b4b5015622fe1d7360a5fc4ee1
yomekamoi <himejike@gmail.com>
2020-08-04 05:13:30 +0000

    商品一覧

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
 	belongs_to :genre
 	belongs_to :genre
 
+	enum is_status: {販売中: 0,販売停止中: 1}
 	validates :name, presence: true
 	validates :introduction, length: { maximum: 200 }
 end
