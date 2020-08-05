83c0db937d0811949ffe4cf383bd05d9d92a2899
ryotafff <jnblog2019@gmail.com>
2020-08-05 06:43:51 +0000

    Update Admins oders index

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,22 +1,32 @@
 class Admins::OrdersController < ApplicationController
 
 
-   def index
-    @path = Rails.application.routes.recognize_path(request.referer)
-    if @path[:controller] == "admin/members" && @path[:action] == "show"
-       @order = Order.where(member_id: params[:format]).page(params[:page]).per(7)
-    elsif @path[:controller] == "admin/admins"
-       @order = Order.where(created_at: Time.zone.today.all_day).page(params[:page]).per(7)
-    else
-       @order = Order.page(params[:page]).per(7)
-    end
 
+
+
+def index
+	# 遷移元(直前)のcontrollerやaction名を取得する
+    @path = Rails.application.routes.recognize_path(request.referer)
+     # 指定したcontrollerとaction名が正しい時、与えられた条件にマッチするものを取得する。
+    if @path[:controller] == "admin/members" && @path[:action] == "show"
+       @orders = Order.where(member_id: params[:format]).page(params[:page]).per(7)
+    elsif @path[:controller] == "admin/admins"
+       @orders = Order.where(created_at: Time.zone.today.all_day).page(params[:page]).per(7)
+    else
+       @orders = Order.page(params[:page]).per(7)
+    end
   end
+
+
 
 
   def show
 
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
   end
+
+
 
   def update
 
