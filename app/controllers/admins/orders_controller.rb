4bf09d4dc6af5eacbb03f6ec2a9584b23d517972
ryotafff <jnblog2019@gmail.com>
2020-08-05 04:46:09 +0000

    Add Admins oders index

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,10 +1,37 @@
 class Admins::OrdersController < ApplicationController
-  def index
+
+
+   def index
+    @path = Rails.application.routes.recognize_path(request.referer)
+    if @path[:controller] == "admin/members" && @path[:action] == "show"
+       @order = Order.where(member_id: params[:format]).page(params[:page]).per(7)
+    elsif @path[:controller] == "admin/admins"
+       @order = Order.where(created_at: Time.zone.today.all_day).page(params[:page]).per(7)
+    else
+       @order = Order.page(params[:page]).per(7)
+    end
+
   end
 
+
   def show
+
   end
 
   def update
+
+
   end
+
+
+
+
+private
+
+  def order_params
+  	params.require(:order).permit(:order_status)
+  end
+
+
+
 end
