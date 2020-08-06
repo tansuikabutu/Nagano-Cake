51fac3453ef9ac3fb47b059bb5d1c12bc00c4b85
ryotafff <jnblog2019@gmail.com>
2020-08-06 05:40:41 +0000

    Add admins order show 途中

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,19 +1,20 @@
 class Admins::OrdersController < ApplicationController
-
-
-
+before_action :authenticate_admin!
 
 
 def index
+
+   # @orders = Order.all
+   
 	# 遷移元(直前)のcontrollerやaction名を取得する
     @path = Rails.application.routes.recognize_path(request.referer)
      # 指定したcontrollerとaction名が正しい時、与えられた条件にマッチするものを取得する。
     if @path[:controller] == "admin/members" && @path[:action] == "show"
-       @orders = Order.where(member_id: params[:format]).page(params[:page]).per(7)
+       @order = Order.where(member_id: params[:format]).page(params[:page]).per(7)
     elsif @path[:controller] == "admin/admins"
-       @orders = Order.where(created_at: Time.zone.today.all_day).page(params[:page]).per(7)
+       @order = Order.where(created_at: Time.zone.today.all_day).page(params[:page]).per(7)
     else
-       @orders = Order.page(params[:page]).per(7)
+       @order = Order.page(params[:page]).per(7)
     end
   end
 
@@ -38,8 +39,6 @@ def index
 	     end
   		  redirect_to  admin_order_path(@order) #注文詳細に遷移
   end
-
-
 
 
 private
--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ Item.create!(
     name: "いちごのショートケーキ",
     introduction: "長野県産の苺を贅沢に使用した当店一押しの商品です",
     price: 1000,
-    is_status: 0,
+    is_status: true,
     image: open("./db/images/image1.jpg")
   },
   {
@@ -56,7 +56,7 @@ Item.create!(
     name: "抹茶のケーキ",
     introduction: "長野松本茶店さんから取り寄せた抹茶を贅沢に使用。抹茶好きにはたまらない一品です。",
     price: 700,
-    is_status: 0,
+    is_status: true,
     image: open("./db/images/image2.jpg")
   },
   {
@@ -65,7 +65,7 @@ Item.create!(
     name: "林檎ぷりん",
     introduction: "長野県産の林檎をそのままプリンの器に。見た目も可愛い一品です。",
     price: 600,
-    is_status: 0,
+    is_status: true,
     image: open("./db/images/image3.jpg")
   },
   {
@@ -74,7 +74,7 @@ Item.create!(
     name: "ブルーベリークッキー",
     introduction: "長野県産のブルーベリーを使用したソフトクッキーです。８枚入りでお届けします。コーヒーのお供にぜひ！",
     price: 800,
-    is_status: 0,
+    is_status: true,
     image: open("./db/images/image4.jpg")
   },
   {
@@ -83,7 +83,7 @@ Item.create!(
     name: "ティラミス",
     introduction: "長野安曇野牧場の濃厚マスカルポーネチーズを使用。ほろ苦いコーヒーとフレッシュなマスカルポーネのハーモニーを楽しめる一品。",
     price: 500,
-    is_status: 0,
+    is_status: true,
     image: open("./db/images/image5.jpg")
   }
  ]
@@ -150,6 +150,34 @@ Item.create!(
  ]
  )
 
+Order.create!(
+[
+  {
+id: 1,
+member_id: 1,
+postage:  800,
+is_payment_method: false,
+address: '北岡県乙西川市馬城寺1-1-1',
+postcode: 111111,
+name: '例田 二郎',
+total_price: 1200,
+order_status:  0,
+}
+]
+)
+
+OrderItem.create!(
+[
+  {
+id: 1,
+order_id: 1,
+item_id: 1,
+perchase_price: 2000,
+quantity: 1,
+make_status: 0,
+ }
+]
+)
 
 
 
