ce22efa5cd5676ec94737278e3a54f448e8e53ba
ryotafff <jnblog2019@gmail.com>
2020-08-06 11:53:28 +0000

    Update admin order item controller

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -7,12 +7,19 @@ def update
  @order_item.update(order_item_params)
 
 
+#製作ステータスが「製作作中」なら注文ステータスを「製作中」に更新
+
 if @order_item.make_status == "製作中"
   @order.update(order_status: 2)
- end
+
+# 上記の条件に当てはまらなければ、商品の個数の特定製作ステータスが「製作完了」の商品をカウント。
+# 注文ステータスを「発送準備中]に更新
+  elsif @order.order_items.count ==  @order.order_items.where(make_status: "製作完了").count
+		@order.update(order_status: 3)
+  end
+   redirect_to admins_order_path(@order)
 
 end
-
 
 
 
