5f8d05d0fb02ef5e7c56dc9ca30f63a1c97aad07
mihomatsuei <addictly_m@yahoo.co.jp>
2020-08-09 12:56:57 +0000

    Fix genre_edit.html

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -7,28 +7,20 @@ def update
  @order = @order_item.order  #注文商品から注文テーブルの呼び出し
  @order_item.update(order_item_params)
 
-	if @order_item.make_status == "製作中"  #製作ステータスが「製作作中」なら入る
+ if @order_item.make_status == "製作中"  #製作ステータスが「製作作中」なら入る
 	   @order.update(order_status: 2) #注文ステータスを「製作中」に更新
 
  elsif @order_item.make_status == "製作完了"   #製作ステータスが「製作完了」なら入る
     @order.update(order_status: 3) #注文ステータスを「発送準備中」に更新
-  end
- 
+ end
+
    redirect_to admins_order_path(@order) #注文詳細に遷移
-
-
 end
-
-
-
-
 
 private
 
   def order_item_params
     params.require(:order_item).permit(:make_status)
   end
-
-
 
 end
